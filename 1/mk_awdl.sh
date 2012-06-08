7b0b064b263552532ce5c92468bb63f48c5eea4a
Sebastian Heath <sebastian.heath@gmail.com>
2012-06-07 20:21:58 -0400

    xhtml?

--- 1/mk_awdl.sh
+++ 1/mk_awdl.sh
@@ -1,1 +1,1 @@
-xsltproc --path "." --novalid --stringparam 'autoinit' '' ../etc/isaw-papers2awdl.xsl ../../isaw-papers/isaw-papers-1/isaw-papers-1.html  > index.html
+xsltproc --path "." --novalid --stringparam 'autoinit' '' ../etc/isaw-papers2awdl.xsl ../../isaw-papers/isaw-papers-1/isaw-papers-1.html  > index.xhtml
