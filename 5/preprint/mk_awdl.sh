864b24b0a18b9ada494e9db4407c7a2c52d24a4a
Sebastian Heath <sebastian.heath@gmail.com>
2012-06-30 09:02:32 -0400

    xhtml

--- 5/preprint/mk_awdl.sh
+++ 5/preprint/mk_awdl.sh
@@ -1,2 +1,2 @@
-xsltproc --path "." --stringparam 'autoinit' 'autoinit' --novalid ../../etc/isaw-papers2awdl.xsl ../../../isaw/isaw-papers/isaw-papers-5/isaw-papers-5.html > index.xhtml 
+xsltproc --path "." --stringparam 'autoinit' 'autoinit' --novalid ../../etc/isaw-papers2awdl.xsl ../../../isaw/isaw-papers/isaw-papers-5/isaw-papers-5.xhtml > index.xhtml 
 cp ../../../isaw/isaw-papers/isaw-papers.css .
