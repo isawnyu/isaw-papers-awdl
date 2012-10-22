c267f3ecbb8286c528a5e201d3949fcb74816262
Sebastian Heath <sebastian.heath@gmail.com>
2012-10-22 15:17:14 -0400

    finishing 3

--- 3/preprint/mk_awdl.sh
+++
@@ -1,2 +0,0 @@
-xsltproc --path "." --stringparam 'autoinit' 'autoinit' --novalid ../../etc/isaw-papers2awdl.xsl ../../../isaw/isaw-papers/isaw-papers-3/isaw-papers-3.html > index.xhtml 
-cp ../../../isaw/isaw-papers/isaw-papers.css .
