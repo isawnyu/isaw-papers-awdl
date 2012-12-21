ce76683e8d34b9f45e68770ab99bd0e1ba8b2714
Sebastian Heath <sfsh@sfsh-macbook.local>
2012-12-21 12:22:56 -0500

    4 to permanent

--- 4/preprint/mk_awdl.sh
+++
@@ -1,2 +0,0 @@
-xsltproc --path "." --stringparam 'autoinit' 'autoinit' --novalid ../../etc/isaw-papers2awdl.xsl ../../../isaw/isaw-papers/isaw-papers-4/isaw-papers-4.html > index.xhtml 
-cp ../../../isaw/isaw-papers/isaw-papers.css .
