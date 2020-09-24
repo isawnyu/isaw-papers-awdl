fbf67225e4a84a4e72b4a7c2cdc54accc5be8815
Sebastian Heath <sebastian.heath@gmail.com>
2020-09-24 15:57:32 -0400

    20-13 for realz

--- html_offprints.py
+++ html_offprints.py
@@ -161,7 +161,7 @@ def video(soup):
 # build dir array
 
 papers_list = ['18','18-1','18-2','18-3','18-4','18-5','18-6','18-7','18-8','18-9','18-10','18-11','19']
-papers_list = ['20','20-1','20-2','20-3','20-4','20-5','20-6','20-7','20-8','20-9','20-11','20-12']
+papers_list = ['20','20-1','20-2','20-3','20-4','20-5','20-6','20-7','20-8','20-9','20-11','20-12','20-13']
 
 for j in papers_list :
     # Opening every article and creating a BeautifulSoup object with all the modifications
